e6391390051c0972dd9dbe0df8c3ba4982ebea0a
fiftycentsjj <fiftycentsjj@gmail.com>
2025-12-12 19:29:57 +0800

    Initial Commit

---
+++ app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by Macbook_PRO on 2025/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Macbook_PRO on 2025/12/12.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
